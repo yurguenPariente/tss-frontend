f3141b31143a661767e7df6390660b02ef3d84e5
yurguenPariente <201708315@est.umss.edu>
2021-11-10 16:00:16 -0400

    Arreglando direccion de la interfaz de ventas

--- src/app/vistas/costos/costos.page.ts
+++ src/app/vistas/costos/costos.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { VentasMes } from '../../interfaces/ventasMensuales';
+import { VentasMes } from 'src/app/interfaces/ventasMensuales';
+
 
 interface Option {
   value:string;
