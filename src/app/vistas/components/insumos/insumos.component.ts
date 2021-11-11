a86604422dca548f156ddb2e86aa0fcaa04517ed
yurguenPariente <201708315@est.umss.edu>
2021-11-11 15:10:34 -0400

    Anadiendo formularios reactivos dinamicos a los formualrios de insumos

--- src/app/vistas/components/insumos/insumos.component.ts
+++ src/app/vistas/components/insumos/insumos.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, FormControl, Validators, FormArray } from '@angular/forms';
 
 interface Option {
-  value:string;
+  value:number;
   viewValue:string;
 }
 @Component({
@@ -12,23 +12,64 @@ interface Option {
 })
 export class InsumosComponent implements OnInit {
   opciones: Option[] = [
-    {value:'25',viewValue:'Diario'},
-    {value:'4',viewValue:'Semanal'},
-    {value:'2',viewValue:'Quincenal'},
-    {value:'1',viewValue:'Mensual'},
-    {value:'0,5',viewValue:'Bimentral'},
-    {value:'0,3333333333',viewValue:'Trimestral'},
-    {value:'0,1666666667',viewValue:'Semestral'},
+    {value:25,viewValue:'Diario'},
+    {value:4,viewValue:'Semanal'},
+    {value:2,viewValue:'Quincenal'},
+    {value:1,viewValue:'Mensual'},
+    {value:0.5,viewValue:'Bimentral'},
+    {value:0.3333333333,viewValue:'Trimestral'},
+    {value:0.1666666667,viewValue:'Semestral'},
   ];
   arreglo: number[]=[];
+
   miFormulario: FormGroup = this.fb.group({
-    cantidad2: new FormControl('',[Validators.required])
+    cantidad2: new FormControl(1,Validators.required),
+    tipo: new FormControl('', Validators.required),
+    cantidad1: new FormControl(0,Validators.required),
+    unidadV: new FormControl('', Validators.required),
+    frecuencia: new FormControl(0,Validators.required),
+    precioV: new FormControl(0, Validators.required),
+    insumos: this.fb.array([
+      this.fb.group({
+        insumo:new FormControl('', Validators.required),
+        cantidad:new FormControl(0,Validators.required),
+        unidadesProd:new FormControl(0, Validators.required),
+        precioU:new FormControl(0, Validators.required)
+      })
+    ])
   })
   constructor(public fb: FormBuilder) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 
-  llenarArreglo(){
-    this.arreglo = Array(this.miFormulario.get('cantidad2').value).fill(1);
+  get insumos(): FormArray{
+    return this.miFormulario.get('insumos') as FormArray;
+  }
+
+  modificarArreglo(){
+    if(this.insumos.length > this.miFormulario.get('cantidad2').value){
+      this.eliminar();
+      console.log(this.insumos.value);
+    }else{
+      this.addInsumo();
+      console.log(this.insumos.value);
+
+    }
+
+  }
+
+  addInsumo(){
+
+      const insumo = this.fb.group({
+      insumo:new FormControl('', Validators.required),
+      cantidad:new FormControl(0,Validators.required),
+      unidadesProd:new FormControl(0, Validators.required),
+      precioU:new FormControl(0, Validators.required)
+    });
+    this.insumos.push(insumo);
+  }
+  eliminar(){
+    this.insumos.removeAt(this.insumos.length-1);
   }
 }
