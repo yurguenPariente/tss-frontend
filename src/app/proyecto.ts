edbb10856a48ea265fe5ccddac4c7e802d01a284
JMauricioQuinonesVargas <201702063@est.umss.edu>
2021-11-11 15:01:58 -0400

    Cambiado formulario de datos a reactivo y agregado verificacion

---
+++ src/app/proyecto.ts
@@ -0,0 +1,7 @@
+export interface Proyecto {
+    nombre:string;
+    representante:string;
+    direccion:string;
+    nit:string;
+
+}
--- src/app/vistas/detallers-proyecto/detallers-proyecto.page.ts
+++ src/app/vistas/detallers-proyecto/detallers-proyecto.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Proyecto } from 'src/app/proyecto';
 
 @Component({
   selector: 'app-detallers-proyecto',
@@ -7,12 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetallersProyectoPage implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup;
+
+  proyecto: Proyecto = {
+    nombre:'',
+    representante:'',
+    direccion:'',
+    nit:'',
+  }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.miFormulario= this.fb.group({
+      nombre:[this.proyecto.nombre, Validators.required],
+      representante:[this.proyecto.representante,[Validators.required]],
+      direccion:[this.proyecto.direccion,[Validators.required]],      
+      nit:[this.proyecto.nit,[Validators.required]],      
+    })
   }
 
-  guardarDatos(nom: HTMLInputElement, rep: HTMLInputElement, dir: HTMLInputElement, nit: HTMLInputElement) {
-    console.log("Nombre del proyecto: "+nom.value+" , representante legal: "+rep.value+" ,direccion: "+dir.value+" ,NIT: "+nit.value);    
+  guardarDatos() {
+    //console.log("Nombre del proyecto: "+nom.value+" , representante legal: "+rep.value+" ,direccion: "+dir.value+" ,NIT: "+nit.value);   
+    this.proyecto = {
+      nombre :this.miFormulario.get('nombre').value,
+      representante : this.miFormulario.get('representante').value,
+      direccion:this.miFormulario.get('direccion').value,
+      nit: this.miFormulario.get('nit').value,    
+    } 
+    console.log(this.proyecto)
   }
 }
