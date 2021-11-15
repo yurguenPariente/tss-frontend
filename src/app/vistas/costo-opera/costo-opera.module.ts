440835a709cd0f0e930651e2dc681507fcd0362e
yurguenPariente <201708315@est.umss.edu>
2021-11-15 10:05:45 -0400

    Arreglo en vistas de simulacion y carga de datos del localstorage

--- src/app/vistas/costo-opera/costo-opera.module.ts
+++ src/app/vistas/costo-opera/costo-opera.module.ts
@@ -8,12 +8,14 @@ import { IonicModule } from '@ionic/angular';
 import { CostoOperaPageRoutingModule } from './costo-opera-routing.module';
 
 import { CostoOperaPage } from './costo-opera.page';
+import { MaterialModule } from '../../material/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    MaterialModule,
     CostoOperaPageRoutingModule,
    ReactiveFormsModule
   ],
