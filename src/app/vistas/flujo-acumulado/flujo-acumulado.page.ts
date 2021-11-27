46c105ad7e75c0cada9957f34d90eb4be1d6ba68
THEBESTgit <55772598+THEBESTgit@users.noreply.github.com>
2021-11-27 11:35:20 -0400

    asd

--- src/app/vistas/flujo-acumulado/flujo-acumulado.page.ts
+++ src/app/vistas/flujo-acumulado/flujo-acumulado.page.ts
@@ -1,4 +1,5 @@
 import { AfterContentInit, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SimulacionService } from '../services/simulacion.service';
 export interface Opcion {
   nombre:string;
@@ -15,6 +16,7 @@ export class FlujoAcumuladoPage implements OnInit, AfterContentInit {
     total: number = JSON.parse(localStorage.getItem('ventasMes')).total;
     costosA: number = Number(localStorage.getItem('costosAnual'));
     totalGas: number = JSON.parse(localStorage.getItem('CostoOp')).total;
+    //monto: number = Number(localStorage.getItem('monto'));
     resultadoVan:number = 0;
     exito:number = 0;
     fracaso: number = 0;
@@ -28,9 +30,15 @@ export class FlujoAcumuladoPage implements OnInit, AfterContentInit {
       {nombre:'Utilidad Neta',valor:(this.total-this.costosA)-((this.totalGas)*12)},
       {nombre:'Cuota',valor:60361},
       {nombre:'Flujo Acumulado',valor:(this.total-this.costosA)-((this.totalGas)*12)-60361}
+
     
   ];
-  constructor(private simulacionService:SimulacionService) { }
+  miFormulario: FormGroup= this.fb.group({
+    //monto:[ 0, [ Validators.max(1000000)]],
+    tipo:[""]
+  })
+  
+  constructor(private simulacionService:SimulacionService,private fb:FormBuilder) { }
 
   ngAfterContentInit(): void {
 
